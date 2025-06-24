6c1f01ae5b7e4d3493b4df11608691146d52ecdd
Oh Juntaek <ojt8416@gmail.com>
2025-06-24 11:42:07 +0900

    hello c

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello git\n");
+	printf("hello intel\n");
+	printf("hello.kccl\n");
+	printf("hello.world\n");
+	printf("hello.gcc\n");
+
+	return 0;
+}
